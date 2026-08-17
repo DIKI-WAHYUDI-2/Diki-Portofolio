e7e626d1c65be24c1fd72c8f476ce71b36b54c74
chardoxx-3 <183924151+chardoxx-3@users.noreply.github.com>
2026-08-17 10:19:48 +0800

    Remove certificate border line and update experience images

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -173,8 +173,10 @@ const EXPERIENCE = [
     desc: "Participated in a team-based digital solution development activity focused on addressing the needs of local MSMEs and the tourism sector. Contributed to the development and presentation of an online platform designed to showcase and promote local MSME products. Gained experience in collaborative problem-solving, digital solution design, and pitching technology-based solutions.",
     certificate: "Certificate of Appreciation",
     images: [
+      "/images/experience/hackforgov/1.jpg",
       "/images/experience/hackforgov/2.jpg",
       "/images/experience/hackforgov/3.jpg",
+      "/images/experience/hackforgov/4.jpg",
       "/images/experience/hackforgov/5.jpg",
     ],
   },
