c83c72b7e9accd6aaa5f0e3afdce0dcce3898676
chardoxx-3 <183924151+chardoxx-3@users.noreply.github.com>
2026-08-19 21:53:11 +0800

    Updated Files

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -2,21 +2,16 @@
 
 import React from "react";
 import Header from "../../components/Header";
-import FeaturedProjects from "../../components/FeaturedProjects";
 import TechnicalArchive from "../../components/TechnicalArchive";
 import WorkCTA from "../../components/WorkCTA";
 import { PROJECTS, TECHNICAL_CATEGORIES } from "../../lib/projects";
 
-const featuredIds = ["02", "03", "01", "04"];
-const featured = PROJECTS.filter((p) => featuredIds.includes(p.num));
 const technical = PROJECTS.filter((p) => !p.category.includes("Digital"));
 
 export default function Work() {
   return (
     <main style={{ position: "relative" }}>
       <Header alwaysShowCoffee alwaysShowBackToTop />
-
-      <FeaturedProjects projects={featured} />
 
       <TechnicalArchive
         projects={technical}
