7ec1171aefabd4f84973abebe21d6c4483a5b58e
chardoxx-3 <183924151+chardoxx-3@users.noreply.github.com>
2026-08-20 16:25:44 +0800

    Updated Files

--- components/DigitalGallery.jsx
+++ components/DigitalGallery.jsx
@@ -21,8 +21,11 @@ export default function DigitalGallery({ projects = [] }) {
             <img src={hero.image} alt={hero.title} className="digital-image" />
           </div>
           <div className="digital-content">
-            <h3 className="digital-title">{hero.title}</h3>
-            <p className="digital-desc">{hero.desc}</p>
+            <div className="digital-content-inner">
+              <div className="digital-num-bg">{hero.num}</div>
+              <h3 className="digital-title">{hero.title}</h3>
+              <p className="digital-desc">{hero.desc}</p>
+            </div>
           </div>
         </div>
       )}
